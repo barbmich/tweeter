c7860f8ca8e2df88fc1fa2d2ef0f8a232e316047
Michele Barbiero <babishhh@gmail.com>
2020-08-11 23:25:45 +0000

    updated css

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,7 +1,6 @@
 $(document).ready(function() {
 
-
-  // 
+  // updates the character counter. if the value goes below 0, a new class is added to it.
   $('#tweet-text').on('input', function () {
     const output = $("#tweet-form").find('output');
     let counter = 140 - $(this).val().length;
