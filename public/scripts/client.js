4c46bbf63818d0034f62fbb9d3487838df40ce0f
Michele Barbiero <babishhh@gmail.com>
2020-08-12 22:00:44 +0000

    add escaping on user content input

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,46 +4,16 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-// Test / driver code (temporary). Eventually will get this from the server.
-
-// const tweetData = {
-//   "user": {
-//     "name": "Newton",
-//     "avatars": "https://i.imgur.com/73hZDYK.png",
-//     "handle": "@SirIsaac"
-//   },
-//   "content": {
-//     "text": "If I have seen further it is by standing on the shoulders of giants"
-//   },
-//   "created_at": 1461116232227
-// }
-
-// const data = [
-//   {
-//     "user": {
-//       "name": "Newton",
-//       "avatars": "https://i.imgur.com/73hZDYK.png"
-//       ,
-//       "handle": "@SirIsaac"
-//     },
-//     "content": {
-//       "text": "If I have seen further it is by standing on the shoulders of giants"
-//     },
-//     "created_at": 1461116232227
-//   },
-//   {
-//     "user": {
-//       "name": "Descartes",
-//       "avatars": "https://i.imgur.com/nlhLi3I.png",
-//       "handle": "@rd" },
-//     "content": {
-//       "text": "Je pense , donc je suis"
-//     },
-//     "created_at": 1461113959088
-//   }
-// ]
+const escape = function(string) {
+  let div = document.createElement('div');
+  div.appendChild(document.createTextNode(string));
+  return div.innerHTML;
+}
 
 const createTweetElement = function (object) {
+
+  const safeHTML = escape(object.content.text);
+
   let tweet =
     `<article class="tweet">
       <header>
@@ -51,7 +21,7 @@ const createTweetElement = function (object) {
         <span class="name">${object.user.name}</span>
         <span class="handle">${object.user.handle}</span>
       </header>
-      <p>${object.content.text}</p>
+      <p>${safeHTML}</p>
       <footer>
         <span>${object.created_at}</span>
         <span>icons</span>
@@ -68,8 +38,9 @@ const renderTweets = function(tweets) {
 
 $(document).ready(function() {
 
+  const apiURL = '/tweets';
+
   const loadTweets = () => {
-    const apiURL = '/tweets';
     $.ajax({method: 'GET', url: apiURL}).then((response) => {
       renderTweets(response);
     })
@@ -78,9 +49,24 @@ $(document).ready(function() {
 
   $('#tweet-form').on('submit', function(evt) {
     evt.preventDefault();
-    const apiURL = '/tweets';
-    const data = $(this).serialize();
-    $.ajax({method: 'POST', url: apiURL, data: data})
-  })
 
-});
+    let currentContent = $('#tweet-text').val();
+    console.log(currentContent);
+
+    if (currentContent.length === 0) {
+      return alert("enter something first!");
+    }
+    if (currentContent > 140) {
+      return alert("too many chars");
+    }
+
+    const data = $(this).serialize();
+    console.log(data);
+
+    $.ajax({method: 'POST', url: apiURL, data: data})
+
+    $('#tweets-container').empty();
+    loadTweets();
+
+  })
+})
