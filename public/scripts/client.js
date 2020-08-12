60099e6b273239ed886dd8e944e28d875f1bd997
Michele Barbiero <babishhh@gmail.com>
2020-08-12 19:03:57 +0000

    implement AJAX POST request for new tweets

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,9 +4,7 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-// const { response } = require("express");
-
- // Test / driver code (temporary). Eventually will get this from the server.
+// Test / driver code (temporary). Eventually will get this from the server.
 
 // const tweetData = {
 //   "user": {
@@ -20,30 +18,30 @@
 //   "created_at": 1461116232227
 // }
 
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-]
+// const data = [
+//   {
+//     "user": {
+//       "name": "Newton",
+//       "avatars": "https://i.imgur.com/73hZDYK.png"
+//       ,
+//       "handle": "@SirIsaac"
+//     },
+//     "content": {
+//       "text": "If I have seen further it is by standing on the shoulders of giants"
+//     },
+//     "created_at": 1461116232227
+//   },
+//   {
+//     "user": {
+//       "name": "Descartes",
+//       "avatars": "https://i.imgur.com/nlhLi3I.png",
+//       "handle": "@rd" },
+//     "content": {
+//       "text": "Je pense , donc je suis"
+//     },
+//     "created_at": 1461113959088
+//   }
+// ]
 
 const createTweetElement = function (object) {
   let tweet =
@@ -70,6 +68,11 @@ const renderTweets = function(tweets) {
 
 $(document).ready(function() {
 
-  renderTweets(data);
+  $('#tweet-form').on('submit', function(evt) {
+    evt.preventDefault();
+    let apiURL = '/tweets';
+    const data = $(this).serialize();
+    $.ajax({method: 'POST', url: apiURL, data: data})
+  })
 
 });
