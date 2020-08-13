c90a5a5edf07114d368e8402df895587364f18f3
Michele Barbiero <babishhh@gmail.com>
2020-08-13 01:20:59 +0000

    add momentJS package for date handling

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,31 +4,11 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-const data = {
-  "user": {
-    "name": "Newton",
-    "avatars": "https://i.imgur.com/73hZDYK.png",
-      "handle": "@SirIsaac"
-    },
-  "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-  "created_at": 1461116232227
-}
-
-const escape = function(string) {
+const escape = function (string) {
   let div = document.createElement('div');
   div.appendChild(document.createTextNode(string));
   return div.innerHTML;
 }
-
-const tweetDate = (currentDate, postDate) {
-  let difference = currentDate - postDate;
-  return difference / (1000 * 3600 * 24);
-}
-
-console.log(tweetDate(Date.now(), data.created_at);
-
 
 const createTweetElement = function (object) {
 
@@ -43,43 +23,43 @@ const createTweetElement = function (object) {
       </header>
       <p>${safeHTML}</p>
       <footer>
-        <span>${object.created_at}</span>
+        <span>${moment(object.created_at).fromNow()}</span>
         <span>icons</span>
       </footer>
     </article>`
   return tweet;
 }
 
-const renderTweets = function(tweets) {
-  const sortedTweets = tweets.sort((a,b) => (b.created_at - a.created_at));
+const renderTweets = function (tweets) {
+  const sortedTweets = tweets.sort((a, b) => (b.created_at - a.created_at));
   for (const tweet of sortedTweets) {
     $('#tweets-container').append(createTweetElement(tweet));
   }
 };
 
-const createAlertBox = function(issue) {
+const createAlertBox = function (issue) {
 
-  let alert = 
+  let alert =
     `<article id="alert">
       <span>${issue}</span>
     </article>`;
 
   return $('.new-tweet').prepend(alert);
-   
+
 }
 
-$(document).ready(function() {
+$(document).ready(function () {
 
   const apiURL = '/tweets';
 
   const loadTweets = () => {
-    $.ajax({method: 'GET', url: apiURL}).then((response) => {
-        renderTweets(response);
+    $.ajax({ method: 'GET', url: apiURL }).then((response) => {
+      renderTweets(response);
     })
   };
   loadTweets();
 
-  $('#tweet-form').on('submit', function(evt) {
+  $('#tweet-form').on('submit', function (evt) {
     evt.preventDefault();
     $('#alert').slideUp("slow");
 
@@ -97,7 +77,7 @@ $(document).ready(function() {
 
     const data = $(this).serialize();
 
-    $.ajax({method: 'POST', url: apiURL, data: data})
+    $.ajax({ method: 'POST', url: apiURL, data: data })
       .then(() => $('#tweets-container').empty())
       .then(loadTweets);
 
