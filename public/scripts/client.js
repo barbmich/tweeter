b6dbc7f8ed7290ef76a7239dfe6fa44bd950ff1e
Michele Barbiero <babishhh@gmail.com>
2020-08-13 00:00:17 +0000

    add error message handling

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,11 +4,31 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+const data = {
+  "user": {
+    "name": "Newton",
+    "avatars": "https://i.imgur.com/73hZDYK.png",
+      "handle": "@SirIsaac"
+    },
+  "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+  "created_at": 1461116232227
+}
+
 const escape = function(string) {
   let div = document.createElement('div');
   div.appendChild(document.createTextNode(string));
   return div.innerHTML;
 }
+
+const tweetDate = (currentDate, postDate) {
+  let difference = currentDate - postDate;
+  return difference / (1000 * 3600 * 24);
+}
+
+console.log(tweetDate(Date.now(), data.created_at);
+
 
 const createTweetElement = function (object) {
 
@@ -31,10 +51,22 @@ const createTweetElement = function (object) {
 }
 
 const renderTweets = function(tweets) {
-  for (const tweet of tweets) {
+  const sortedTweets = tweets.sort((a,b) => (b.created_at - a.created_at));
+  for (const tweet of sortedTweets) {
     $('#tweets-container').append(createTweetElement(tweet));
   }
 };
+
+const createAlertBox = function(issue) {
+
+  let alert = 
+    `<article id="alert">
+      <span>${issue}</span>
+    </article>`;
+
+  return $('.new-tweet').prepend(alert);
+   
+}
 
 $(document).ready(function() {
 
@@ -42,31 +74,32 @@ $(document).ready(function() {
 
   const loadTweets = () => {
     $.ajax({method: 'GET', url: apiURL}).then((response) => {
-      renderTweets(response);
+        renderTweets(response);
     })
   };
   loadTweets();
 
   $('#tweet-form').on('submit', function(evt) {
     evt.preventDefault();
+    $('#alert').slideUp("slow");
 
     let currentContent = $('#tweet-text').val();
-    console.log(currentContent);
 
     if (currentContent.length === 0) {
-      return alert("enter something first!");
+      $('#alert-text').text("⚠️ You should enter some text! ⚠️")
+      return $('#alert').slideDown("slow");
     }
-    if (currentContent > 140) {
-      return alert("too many chars");
+
+    if (currentContent.length > 140) {
+      $('#alert-text').text("⚠️ That's waaaay to many characters! ⚠️")
+      return $('#alert').slideDown("slow");
     }
 
     const data = $(this).serialize();
-    console.log(data);
 
     $.ajax({method: 'POST', url: apiURL, data: data})
-
-    $('#tweets-container').empty();
-    loadTweets();
+      .then(() => $('#tweets-container').empty())
+      .then(loadTweets);
 
   })
 })
